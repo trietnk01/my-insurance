f3c644aa22aaefafc45a066e4fd6ad53936de87c
diennk <dien@itsabeautifulday.fr>
2022-04-30 19:27:08 +0700

    Update code 30-04-2022-19h30

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,37 @@
 import React from "react";
 import "assets/frontpage/frontpage-main.scss";
+import logo from "assets/images/logo.svg";
+import HomeIcon from "assets/images/home-icon.svg";
 function Home() {
   return (
-    <div className="relative">
+    <div className="relative font-frontpage-global">
       <div className="box-banner flex absolute left-0 top-0 w-full">
-        <div className="box-left"></div>
-        <div className="box-right"></div>
+        <div className="banner-header flex text-white items-center justify-center absolute right-0 top-0" style={{ columnGap: "100px" }}>
+          <div className="item-banner-header">
+            <span className="item-banner-header__text">EN</span>
+          </div>
+          <div className="item-banner-header">
+            <span className="item-banner-header__text">Company</span>
+          </div>
+          <div className="item-banner-header">
+            <span className="item-banner-header__text">Partner Market Place</span>
+          </div>
+        </div>
       </div>
-      <div className="kairos-container relative flex">
-        <div className="col-left"></div>
-        <div className="col-right grow"></div>
+      <div className="kairos-container relative flex mx-auto">
+        <div className="col-left">
+          <div className="flex justify-between items-center">
+            <img src={logo} />
+            <div className="flex" style={{ columnGap: "10px" }}>
+              <img src={HomeIcon} />
+              <span className="front-page-text-home">Home</span>
+            </div>
+          </div>
+          <div className="banner-slogan">
+            Asia IT <br /> Market Place
+          </div>
+        </div>
+        <div className="grow"></div>
       </div>
     </div>
   );
